770af436dc10fc6679eb600880b79e379f5daade
James Price <j.price@bristol.ac.uk>
2015-10-08 16:03:44 +0100

    Initial commit.

---
+++ NBody-Metal/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NBody-Metal
+//
+//  Created by James Price on 08/10/2015.
+//  Copyright © 2015 James Price. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
