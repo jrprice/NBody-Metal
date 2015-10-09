269b04578b268e9b5dca64f6f08d298ebf802168
James Price <j.price@bristol.ac.uk>
2015-10-09 12:25:09 +0100

    Create CommandQueue and objects for rendering.

--- NBody-Metal/NBodyViewController.swift
+++ NBody-Metal/NBodyViewController.swift
@@ -13,17 +13,34 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
   private let WIDTH  = 640
   private let HEIGHT = 480
 
+  private var queue: MTLCommandQueue!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     self.view.window?.setContentSize(NSSize(width: WIDTH, height: HEIGHT))
 
-    let metalview = MTKView(frame: CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT), device: MTLCreateSystemDefaultDevice())
+    let metalview = MTKView(frame: CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT))
     metalview.delegate = self
     view.addSubview(metalview)
+
+    let device = MTLCreateSystemDefaultDevice()!
+    queue = device.newCommandQueue()
+    metalview.device = device
   }
 
   func drawInMTKView(view: MTKView) {
+    let renderPassDescriptor = view.currentRenderPassDescriptor
+    renderPassDescriptor!.colorAttachments[0].loadAction = .Clear
+    renderPassDescriptor!.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.2, 1.0)
+
+    let buffer = queue.commandBuffer()
+    let encoder = buffer.renderCommandEncoderWithDescriptor(renderPassDescriptor!)
+
+    encoder.endEncoding()
+
+    buffer.presentDrawable(view.currentDrawable!)
+    buffer.commit()
   }
 
   func mtkView(view: MTKView, drawableSizeWillChange size: CGSize) {
