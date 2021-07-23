b508ddc1ac654ae0b90c5399809a369a3efb7cb6
James Price <jrprice@google.com>
2021-07-23 14:43:02 -0400

    Update to Swift 5 syntax

--- NBody-Metal/AppDelegate.swift
+++ NBody-Metal/AppDelegate.swift
@@ -8,20 +8,34 @@
 
 import Cocoa
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-  @IBOutlet weak var window: NSWindow!
-
-  func applicationDidFinishLaunching(aNotification: NSNotification) {
-    window.title = "NBody-Metal"
+class Application : NSApplication {
+  let strongDelegate = AppDelegate()
+  override init(){
+    super.init()
+    self.delegate = strongDelegate
   }
 
-  func applicationWillTerminate(aNotification: NSNotification) {
-  }
-
-  func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
-    return true
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
 
+@NSApplicationMain
+class AppDelegate : NSObject, NSApplicationDelegate {
+  var window: NSWindow!
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    window.title = "NBody-Metal"
+
+    var frame = window.frame
+    frame.size.width = 1280
+    frame.size.height = 980
+    window.setFrame(frame, display: true)
+    window.minSize = frame.size
+    window.maxSize = frame.size
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+}
--- NBody-Metal/NBodyViewController.swift
+++ NBody-Metal/NBodyViewController.swift
@@ -8,19 +8,22 @@
 
 import MetalKit
 
-private let WIDTH     = 640
-private let HEIGHT    = 480
-private let RADIUS    = Float(0.8)
+private let WIDTH     = 1280
+private let HEIGHT    = 960
+private let RADIUS    = Float(0.6)
 private let GROUPSIZE = 64 // must be same as GROUPSIZE in shaders.metal
-private let DELTA     = Float(0.0001)
-private let SOFTENING = Float(0.05)
-private let MAXBODIES = 32768
+private let DELTA     = Float(0.000025)
+private let SOFTENING = Float(0.2)
+private let MAXBODIES = 131072
 private let MINBODIES = GROUPSIZE
 
 class NBodyViewController: NSViewController, MTKViewDelegate {
 
+  typealias float3 = SIMD3<Float>
+  typealias float4 = SIMD4<Float>
+
   private var deviceIndex = 0
-  private var nbodies     = 8192
+  private var nbodies     = 16384
 
   private var metalview: MTKView!
 
@@ -64,34 +67,33 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
     view.addSubview(metalview)
 
     // Create status labels
-    nametext    = createInfoText(NSMakeRect(10, CGFloat(HEIGHT)-30, 300, 20))
-    nbodiestext = createInfoText(NSMakeRect(10, CGFloat(HEIGHT)-50, 300, 20))
-    fpstext     = createInfoText(NSMakeRect(10, CGFloat(HEIGHT)-70, 120, 20))
-    flopstext   = createInfoText(NSMakeRect(10, CGFloat(HEIGHT)-90, 120, 20))
+    nametext    = createInfoText(rect: NSMakeRect(10, CGFloat(HEIGHT)-30, 300, 20))
+    nbodiestext = createInfoText(rect: NSMakeRect(10, CGFloat(HEIGHT)-50, 300, 20))
+    fpstext     = createInfoText(rect: NSMakeRect(10, CGFloat(HEIGHT)-70, 120, 20))
+    flopstext   = createInfoText(rect: NSMakeRect(10, CGFloat(HEIGHT)-90, 120, 20))
     metalview.addSubview(nametext)
     metalview.addSubview(nbodiestext)
     metalview.addSubview(fpstext)
     metalview.addSubview(flopstext)
 
-    initMetal(false)
+    initMetal(retainBodies: false)
     initBodies()
   }
 
   func createInfoText(rect: NSRect) -> NSTextField {
     let text = NSTextField(frame: rect)
-    text.editable        = false
-    text.bezeled         = false
-    text.selectable      = false
+    text.isEditable      = false
+    text.isBezeled       = false
+    text.isSelectable    = false
     text.drawsBackground = false
-    text.textColor       = NSColor.whiteColor()
-    text.font            = NSFont.boldSystemFontOfSize(14.0)
+    text.textColor       = NSColor.white
+    text.font            = NSFont.boldSystemFont(ofSize: 14.0)
     text.stringValue     = ""
 
     return text
   }
 
-  func drawInMTKView(view: MTKView) {
-
+  func draw(in view: MTKView) {
     // Update FPS and GFLOP/s counters
     frames += 1
     let now  = getTimestamp()
@@ -110,32 +112,32 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
       lastUpdate = now
     }
 
-    buffer = queue?.commandBuffer()
+    buffer = queue?.makeCommandBuffer()
 
     // Compute kernel
     let groupsize = MTLSizeMake(GROUPSIZE, 1, 1)
     let numgroups = MTLSizeMake(nbodies/GROUPSIZE, 1, 1)
-    let computeEncoder = buffer!.computeCommandEncoder()
-    computeEncoder.setComputePipelineState(computePipelineState)
-    computeEncoder.setBuffer(d_positionsIn, offset: 0, atIndex: 0)
-    computeEncoder.setBuffer(d_positionsOut, offset: 0, atIndex: 1)
-    computeEncoder.setBuffer(d_velocities, offset: 0, atIndex: 2)
-    computeEncoder.setBuffer(d_computeParams, offset: 0, atIndex: 3)
-    computeEncoder.dispatchThreadgroups(numgroups, threadsPerThreadgroup: groupsize)
-    computeEncoder.endEncoding()
+    let computeEncoder = buffer!.makeComputeCommandEncoder()
+    computeEncoder!.setComputePipelineState(computePipelineState)
+    computeEncoder!.setBuffer(d_positionsIn, offset: 0, index: 0)
+    computeEncoder!.setBuffer(d_positionsOut, offset: 0, index: 1)
+    computeEncoder!.setBuffer(d_velocities, offset: 0, index: 2)
+    computeEncoder!.setBuffer(d_computeParams, offset: 0, index: 3)
+    computeEncoder!.dispatchThreadgroups(numgroups, threadsPerThreadgroup: groupsize)
+    computeEncoder!.endEncoding()
 
     // Vertex and fragment shaders
     let renderPassDescriptor = view.currentRenderPassDescriptor
-    renderPassDescriptor!.colorAttachments[0].loadAction = .Clear
+    renderPassDescriptor!.colorAttachments[0].loadAction = .clear
     renderPassDescriptor!.colorAttachments[0].clearColor = MTLClearColorMake(0.0, 0.0, 0.1, 1.0)
-    let renderEncoder  = buffer!.renderCommandEncoderWithDescriptor(renderPassDescriptor!)
-    renderEncoder.setRenderPipelineState(renderPipelineState)
-    renderEncoder.setVertexBuffer(d_positionsOut, offset: 0, atIndex: 0)
-    renderEncoder.setVertexBuffer(d_renderParams, offset: 0, atIndex: 1)
-    renderEncoder.drawPrimitives(.Point, vertexStart: 0, vertexCount: nbodies)
-    renderEncoder.endEncoding()
+    let renderEncoder  = buffer!.makeRenderCommandEncoder(descriptor: renderPassDescriptor!)
+    renderEncoder!.setRenderPipelineState(renderPipelineState)
+    renderEncoder!.setVertexBuffer(d_positionsOut, offset: 0, index: 0)
+    renderEncoder!.setVertexBuffer(d_renderParams, offset: 0, index: 1)
+    renderEncoder!.drawPrimitives(type: .point, vertexStart: 0, vertexCount: nbodies)
+    renderEncoder!.endEncoding()
 
-    buffer!.presentDrawable(view.currentDrawable!)
+    buffer!.present(view.currentDrawable!)
     buffer!.commit()
 
     swap(&d_positionsIn, &d_positionsOut)
@@ -151,11 +153,11 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
     buffer?.waitUntilCompleted()
 
     // Initialise positions uniformly at random on surface of sphere, with no velocity
-    let h_positions  = unsafeBitCast(d_positionsIn!.contents(), UnsafeMutablePointer<Float>.self)
-    let h_velocities = unsafeBitCast(d_velocities!.contents(), UnsafeMutablePointer<Float>.self)
+    let h_positions  = d_positionsIn!.contents().assumingMemoryBound(to: Float.self)
+    let h_velocities = d_velocities!.contents().assumingMemoryBound(to: Float.self)
     for i in 0...(nbodies-1) {
-      let longitude = 2.0 * Float(M_PI) * (Float(rand())/Float(RAND_MAX))
-      let latitude  = acos((2.0 * (Float(rand())/Float(RAND_MAX))) - 1.0)
+      let longitude = 2.0 * Float.pi * Float.random(in: 0..<1)
+      let latitude  = acos((2.0 * Float.random(in: 0..<1)) - 1.0)
       h_positions[i*4 + 0] = RADIUS * sin(latitude) * cos(longitude)
       h_positions[i*4 + 1] = RADIUS * sin(latitude) * sin(longitude)
       h_positions[i*4 + 2] = RADIUS * cos(latitude)
@@ -167,8 +169,8 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
       h_velocities[i*4 + 3] = 0.0
     }
 
-    d_positionsIn?.didModifyRange(NSMakeRange(0, sizeof(float4)*nbodies))
-    d_velocities?.didModifyRange(NSMakeRange(0, sizeof(float4)*nbodies))
+    d_positionsIn?.didModifyRange(0..<(MemoryLayout<float4>.size * nbodies))
+    d_velocities?.didModifyRange(0..<(MemoryLayout<float4>.size * nbodies))
   }
 
   func initMetal(retainBodies: Bool) {
@@ -176,58 +178,58 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
     // Get data from previous device
     let h_positions  = d_positionsIn?.contents()
     let h_velocities = d_velocities?.contents()
-    let buffer       = queue?.commandBuffer()
-    let blitEncoder  = buffer?.blitCommandEncoder()
-    blitEncoder?.synchronizeResource(d_positionsIn!)
-    blitEncoder?.synchronizeResource(d_velocities!)
+    let buffer       = queue?.makeCommandBuffer()
+    let blitEncoder  = buffer?.makeBlitCommandEncoder()
+    blitEncoder?.synchronize(resource: d_positionsIn!)
+    blitEncoder?.synchronize(resource: d_velocities!)
     blitEncoder?.endEncoding()
     buffer?.commit()
     buffer?.waitUntilCompleted()
 
     // Select next device
     let device = MTLCopyAllDevices()[deviceIndex]
-    nametext.stringValue = "Device: \(device.name!) [d]"
+    nametext.stringValue = "Device: \(device.name) [d]"
     nbodiestext.stringValue = "Bodies: \(nbodies) [+/-]"
     metalview.device = device
 
-    queue      = device.newCommandQueue()
-    library    = device.newDefaultLibrary()
+    queue      = device.makeCommandQueue()
+    library    = device.makeDefaultLibrary()
     do {
-      computePipelineState = try device.newComputePipelineStateWithFunction(library.newFunctionWithName("step")!)
+        computePipelineState = try device.makeComputePipelineState(function: library.makeFunction(name: "step")!)
     }
     catch {
       print("Failed to create compute pipeline state")
     }
 
     let renderPipelineStateDescriptor = MTLRenderPipelineDescriptor()
-    renderPipelineStateDescriptor.vertexFunction = library.newFunctionWithName("vert")
-    renderPipelineStateDescriptor.fragmentFunction = library.newFunctionWithName("frag")
-    renderPipelineStateDescriptor.colorAttachments[0].pixelFormat = .BGRA8Unorm
-    renderPipelineStateDescriptor.colorAttachments[0].blendingEnabled = true
-    renderPipelineStateDescriptor.colorAttachments[0].destinationRGBBlendFactor = .One
-    renderPipelineStateDescriptor.colorAttachments[0].destinationAlphaBlendFactor = .One
+    renderPipelineStateDescriptor.vertexFunction = library.makeFunction(name: "vert")
+    renderPipelineStateDescriptor.fragmentFunction = library.makeFunction(name: "frag")
+    renderPipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+    renderPipelineStateDescriptor.colorAttachments[0].isBlendingEnabled = true
+    renderPipelineStateDescriptor.colorAttachments[0].destinationRGBBlendFactor = .one
+    renderPipelineStateDescriptor.colorAttachments[0].destinationAlphaBlendFactor = .one
     do {
-      renderPipelineState = try device.newRenderPipelineStateWithDescriptor(renderPipelineStateDescriptor)
+        renderPipelineState = try device.makeRenderPipelineState(descriptor: renderPipelineStateDescriptor)
     }
     catch {
       print("Failed to create render pipeline state")
     }
 
     // Create device buffers
-    let datasize = sizeof(float4)*nbodies
-    d_positions0 = device.newBufferWithLength(datasize, options: .StorageModeManaged)
-    d_positions1 = device.newBufferWithLength(datasize, options: .StorageModeManaged)
-    d_velocities = device.newBufferWithLength(datasize, options: .StorageModeManaged)
+    let datasize = MemoryLayout<float4>.size * nbodies
+    d_positions0 = device.makeBuffer(length: datasize, options: .storageModeManaged)
+    d_positions1 = device.makeBuffer(length: datasize, options: .storageModeManaged)
+    d_velocities = device.makeBuffer(length: datasize, options: .storageModeManaged)
 
     // Copy data from previous device
     if retainBodies {
       if h_positions != nil {
         memcpy(d_positions0!.contents(), h_positions!, datasize)
-        d_positions0?.didModifyRange(NSMakeRange(0, datasize))
+        d_positions0?.didModifyRange(0..<datasize)
       }
       if h_velocities != nil {
         memcpy(d_velocities!.contents(), h_velocities!, datasize)
-        d_velocities?.didModifyRange(NSMakeRange(0, datasize))
+        d_velocities?.didModifyRange(0..<datasize)
       }
     }
 
@@ -240,30 +242,31 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
       var softening:Float = 0
     }
     var h_computeParams = ComputeParams(nbodies: UInt32(nbodies), delta: DELTA, softening: SOFTENING)
-    d_computeParams = device.newBufferWithBytes(&h_computeParams, length: sizeof(ComputeParams), options: .CPUCacheModeDefaultCache)
+    d_computeParams = device.makeBuffer(bytes: &h_computeParams, length: MemoryLayout<ComputeParams>.size, options: .cpuCacheModeWriteCombined)
 
     // Initialise view-projection matrices
     let vpMatrix = Matrix4()
-    vpMatrix.translate(0.0, y: 0.0, z: -2.0)
-    projectionMatrix = Matrix4.makePerspectiveViewAngle(Matrix4.degreesToRad(55.0), aspectRatio: Float(WIDTH)/Float(HEIGHT), nearZ: 0.1, farZ: 50.0)
-    vpMatrix.multiplyLeft(projectionMatrix)
+    vpMatrix!.translate(0.0, y: 0.0, z: -2.0)
+    projectionMatrix = Matrix4.makePerspectiveViewAngle(Matrix4.degrees(toRad: 55.0), aspectRatio: Float(WIDTH)/Float(HEIGHT), nearZ: 0.1, farZ: 50.0)
+    vpMatrix!.multiplyLeft(projectionMatrix)
 
     var eyePosition = float3(0, 0, 2.0)
 
-    let renderParamsSize = sizeof(matrix_float4x4) + sizeof(Float)*4
-    d_renderParams = device.newBufferWithLength(renderParamsSize, options: .CPUCacheModeDefaultCache)
-    memcpy(d_renderParams.contents(), vpMatrix.raw(), sizeof(matrix_float4x4))
-    memcpy(d_renderParams.contents() + sizeof(matrix_float4x4), &eyePosition, sizeof(float3))
+    let renderParamsSize = MemoryLayout<matrix_float4x4>.size + MemoryLayout<float4>.size
+    d_renderParams = device.makeBuffer(length: renderParamsSize, options: .cpuCacheModeWriteCombined)
+    memcpy(d_renderParams.contents(), vpMatrix!.raw(), MemoryLayout<matrix_float4x4>.size)
+    memcpy(d_renderParams.contents() + MemoryLayout<matrix_float4x4>.size, &eyePosition, MemoryLayout<float3>.size)
   }
 
-  override func keyDown(theEvent: NSEvent) {
+  override func keyDown(with theEvent: NSEvent) {
     switch theEvent.keyCode {
     case 2:
       // Select next device
-      if ++deviceIndex >= MTLCopyAllDevices().count {
+      deviceIndex += 1
+      if deviceIndex >= MTLCopyAllDevices().count {
         deviceIndex = 0
       }
-      initMetal(true)
+      initMetal(retainBodies: true)
     case 15:
       initBodies()
     case 12:
@@ -271,22 +274,22 @@ class NBodyViewController: NSViewController, MTKViewDelegate {
     case 27:
       if nbodies > MINBODIES {
         nbodies /= 2
-        initMetal(false)
+        initMetal(retainBodies: false)
         initBodies()
       }
     case 24:
-      if theEvent.modifierFlags.contains(NSEventModifierFlags.ShiftKeyMask) {
+      if theEvent.modifierFlags.contains(NSEvent.ModifierFlags.shift) {
         if nbodies < MAXBODIES {
           nbodies *= 2
-          initMetal(false)
+          initMetal(retainBodies: false)
           initBodies()
         }
       }
     default:
-      super.keyDown(theEvent)
+      super.keyDown(with: theEvent)
     }
   }
 
-  func mtkView(view: MTKView, drawableSizeWillChange size: CGSize) {
+  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
   }
 }
